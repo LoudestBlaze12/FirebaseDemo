af22df1a56b56debf8cb0f9162de981ee5f37f69
LoudestBlaze12 <laudies@live.com>
2022-03-29 11:59:25 -0400

    navigation added and text updated Firebase Init User Signup Implet

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flash_chat/constants.dart';
+import 'chat_screen.dart';
+
 class RegistrationScreen extends StatefulWidget {
 
   static String id = "/registration";
@@ -10,22 +12,25 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+  bool _saving = false;
 
   @override
   void initState()  {
-    // TODO: implement initState
-     startFirebase();
+     // startFirebase();
 
     super.initState();
   }
 
-  void startFirebase() async {
-    await Firebase.initializeApp();
-    print("Firebase was init properly");
-  }
+  // void startFirebase() async {
+  //   await Firebase.initializeApp();
+  //   print("Firebase was init properly");
+  // }
 
 
-  // final _auth = FirebaseAuth.instance;
+
 
 
 
@@ -39,63 +44,55 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+
+            //Logo Assets
             Container(
               height: 200.0,
               child: Image.asset('images/logo.png'),
             ),
+
+
+            //Visual Spacer
             SizedBox(
               height: 48.0,
             ),
+
+
+            //Email Text Field
             TextField(
               textAlign: TextAlign.center,
               onChanged: (value) {
+                email = value;
                 //Do something with the user input.
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
+              decoration:kInputTextField.copyWith(hintText: "Enter your email"),
             ),
+
+
+            //Visual Spacer
             SizedBox(
               height: 8.0,
             ),
+
+
+            //Password Text Field
             TextField(
+              obscureText: true,
               textAlign: TextAlign.center,
               onChanged: (value) {
                 //Do something with the user input.
+                password = value;
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
+              decoration: kInputTextField.copyWith(hintText: "Enter your password"),
             ),
+
+
+            //Visual Spacer
             SizedBox(
               height: 24.0,
             ),
+
+            //Sign up button
             Padding(
               padding: EdgeInsets.symmetric(vertical: 16.0),
               child: Material(
@@ -103,8 +100,21 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 borderRadius: BorderRadius.all(Radius.circular(30.0)),
                 elevation: 5.0,
                 child: MaterialButton(
-                  onPressed: () {
+                  onPressed: () async{
                     //Implement registration functionality.
+                    print(email);
+
+                    try {
+                      final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+                      if (newUser != null){
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      }
+                    }
+                    catch(e) {
+                      print("We couldn't save it boss");
+                      print(e);
+                    }
+
                   },
                   minWidth: 200.0,
                   height: 42.0,
